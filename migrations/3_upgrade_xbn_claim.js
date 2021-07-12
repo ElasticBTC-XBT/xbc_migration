6738e6d849d88bd1ad8ce194f4d51b5563e0505a
Adsigma <hong@cloudjetkpi.com>
2021-07-12 17:11:23 +0700

    mainnet

--- migrations/3_upgrade_xbn_claim.js
+++ migrations/3_upgrade_xbn_claim.js
@@ -5,7 +5,8 @@ const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
 module.exports = async function (deployer) {
   await deployer.deploy(Utils);
   await deployer.link(Utils, ClaimReward);
-  const proxyAddress = "0x8781413C768f207699D51f42b909c5d6A9D9aD36";
+  // const proxyAddress = "0x8781413C768f207699D51f42b909c5d6A9D9aD36"; testnet
+  const proxyAddress = "0x8AB58dd7aC92Ee5088a34556bED11Be7B74B2ab0"; //mainnet
   await upgradeProxy(proxyAddress, ClaimReward, {
     deployer,
     unsafeAllow: ["external-library-linking"],
