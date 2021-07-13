956a959f1ececce4517a172b337c1a71c8f3cb2d
Adsigma <hong@cloudjetkpi.com>
2021-07-13 10:30:54 +0700

    initialize

--- migrations/2_deploy_xbn_claim.js
+++ migrations/2_deploy_xbn_claim.js
@@ -1,20 +1,17 @@
-const ClaimReward = artifacts.require("ClaimReward");
+const AIStaking = artifacts.require("AIStaking");
 const Utils = artifacts.require("Utils");
 const { deployProxy } = require("@openzeppelin/truffle-upgrades");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(Utils);
-  await deployer.link(Utils, ClaimReward);
+  // await deployer.deploy(Utils);
+  // await deployer.link(Utils, AIStaking);
 
   await deployProxy(
-    ClaimReward,
-    [
-      "0xee67b3c7348cfbf1e17777717ddfaced799febae",
-      "0xd99d1c33f9fc3444f8101754abc46c52416550d1",
-    ],
+    AIStaking,
+    [], // initialize params
     {
       deployer,
-      unsafeAllow: ["external-library-linking"],
+   //   unsafeAllow: ["external-library-linking"],
       initializer: "initialize",
     }
   );
--- migrations/3_upgrade_xbn_claim.js
+++ migrations/3_upgrade_xbn_claim.js
@@ -1,10 +1,10 @@
-const ClaimReward = artifacts.require("ClaimReward");
-const Utils = artifacts.require("Utils");
+const AIStaking = artifacts.require("AIStaking");
+// const Utils = artifacts.require("Utils");
 const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(Utils);
-  await deployer.link(Utils, ClaimReward);
+  // await deployer.deploy(Utils);
+  // await deployer.link(Utils, ClaimReward);
   // const proxyAddress = "0x8781413C768f207699D51f42b909c5d6A9D9aD36"; testnet
   const proxyAddress = "0x8AB58dd7aC92Ee5088a34556bED11Be7B74B2ab0"; //mainnet
   await upgradeProxy(proxyAddress, ClaimReward, {
