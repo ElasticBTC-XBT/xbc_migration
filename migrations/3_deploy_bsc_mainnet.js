da4acf849deaea8035ee732b07f8c4bca322ed71
Adsigma <hong@cloudjetkpi.com>
2021-08-10 23:33:40 +0700

    cake

--- migrations/3_deploy_bsc_mainnet.js
+++ migrations/3_deploy_bsc_mainnet.js
@@ -5,7 +5,7 @@ const { deployProxy } = require("@openzeppelin/truffle-upgrades");
 module.exports = async function (deployer) {
   
   let PANCAKE_ROUTER = "0x10ED43C718714eb63d5aA57B78B54704E256024E"; // pancake v2
-  let CAKE = "0xf73D010412Fb5835C310728F0Ba1b7DFDe88379A"; // cake
+  let CAKE = "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82"; // cake
   let CAKE_MASTER_CHEF = "0x73feaa1eE314F8c655E354234017bE2193C9E24E"; // cake master chef
 
   if (process.env.PANCAKE_ROUTER != undefined) {
